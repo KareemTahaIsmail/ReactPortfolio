df85f5c3753c303bb0e37c0badf441407a93e993
Kareem Ismail <KareemTahaIsmail@gmail.com>
2020-10-18 21:25:27 -0400

    Updates

---
+++ precache-manifest.961b0fc6a52b9b70f8e04a19175a4a32.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f24ca5619dabf522f391eeb1b4693b1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f6a6002f5d49eb210f2a",
+    "url": "/react-portfolio/static/css/main.220a7f42.chunk.css"
+  },
+  {
+    "revision": "97b34e86b957aeb0bfe4",
+    "url": "/react-portfolio/static/js/2.7edeec29.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.7edeec29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a6002f5d49eb210f2a",
+    "url": "/react-portfolio/static/js/main.0bc13de5.chunk.js"
+  },
+  {
+    "revision": "9747c577c393cc8adcd5",
+    "url": "/react-portfolio/static/js/runtime-main.95799ade.js"
+  },
+  {
+    "revision": "429da6ed989ee1f2373de249404bd98c",
+    "url": "/react-portfolio/static/media/KMSE.429da6ed.png"
+  },
+  {
+    "revision": "aa6562ae4a7f8ece2b95311a8a13dd5b",
+    "url": "/react-portfolio/static/media/NID.aa6562ae.png"
+  },
+  {
+    "revision": "56be0227d5f4e9d530340feccb536b5b",
+    "url": "/react-portfolio/static/media/P^3.56be0227.png"
+  },
+  {
+    "revision": "217693f6ef185f0c374af29e503e10ab",
+    "url": "/react-portfolio/static/media/background.217693f6.jpg"
+  },
+  {
+    "revision": "f45f22dd91c8a127a177c2e333ce0e3f",
+    "url": "/react-portfolio/static/media/background1.f45f22dd.jpg"
+  },
+  {
+    "revision": "4fedff8dd6bdc56c8b6169d1850984d3",
+    "url": "/react-portfolio/static/media/background2.4fedff8d.jpg"
+  },
+  {
+    "revision": "211b543fa3cc80297c1299e9a60f968d",
+    "url": "/react-portfolio/static/media/fox.211b543f.gif"
+  }
+]);
