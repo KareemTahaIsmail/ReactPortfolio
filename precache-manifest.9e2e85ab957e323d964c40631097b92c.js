f398b621bd46fe3614f2928b9d4642c0bdad5d20
Kareem Ismail <KareemTahaIsmail@gmail.com>
2020-10-18 21:26:58 -0400

    Updates

---
+++ precache-manifest.9e2e85ab957e323d964c40631097b92c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1834ed4817ea34aafea73051fc5d3e7",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "ac8450b8e4fbee7eb44c",
+    "url": "/ReactPortfolio/static/css/main.4358165f.chunk.css"
+  },
+  {
+    "revision": "97b34e86b957aeb0bfe4",
+    "url": "/ReactPortfolio/static/js/2.7edeec29.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ReactPortfolio/static/js/2.7edeec29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8450b8e4fbee7eb44c",
+    "url": "/ReactPortfolio/static/js/main.c594c1cb.chunk.js"
+  },
+  {
+    "revision": "92428f60f1b68a6c5af6",
+    "url": "/ReactPortfolio/static/js/runtime-main.7366d0c1.js"
+  },
+  {
+    "revision": "429da6ed989ee1f2373de249404bd98c",
+    "url": "/ReactPortfolio/static/media/KMSE.429da6ed.png"
+  },
+  {
+    "revision": "aa6562ae4a7f8ece2b95311a8a13dd5b",
+    "url": "/ReactPortfolio/static/media/NID.aa6562ae.png"
+  },
+  {
+    "revision": "56be0227d5f4e9d530340feccb536b5b",
+    "url": "/ReactPortfolio/static/media/P^3.56be0227.png"
+  },
+  {
+    "revision": "217693f6ef185f0c374af29e503e10ab",
+    "url": "/ReactPortfolio/static/media/background.217693f6.jpg"
+  },
+  {
+    "revision": "f45f22dd91c8a127a177c2e333ce0e3f",
+    "url": "/ReactPortfolio/static/media/background1.f45f22dd.jpg"
+  },
+  {
+    "revision": "4fedff8dd6bdc56c8b6169d1850984d3",
+    "url": "/ReactPortfolio/static/media/background2.4fedff8d.jpg"
+  },
+  {
+    "revision": "211b543fa3cc80297c1299e9a60f968d",
+    "url": "/ReactPortfolio/static/media/fox.211b543f.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.961b0fc6a52b9b70f8e04a19175a4a32.js"
+  "/ReactPortfolio/precache-manifest.9e2e85ab957e323d964c40631097b92c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactPortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
